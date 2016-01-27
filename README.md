29536e48bd0821658585530ff3e5ce95206ccb6e
Bert Spaan <bert.spaan@gmail.com>
2016-01-27 13:39:40 -0500

    api for located items

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NYPL Where API
+# NYPL Labs - Where?
 
 1. `git clone` this repository
 2. `cd` the new folder
@@ -7,12 +7,37 @@
 
 ## API
 
+### Items
+
+- `GET /items`: get all collection items
+- `GET /items/:uuid`: get a single collection item, with UUID `:uuid`
 - `GET /items/random`: get random collection item
-- `POST /items/:uuid`: send new location of item with UUID `:uuid`. POST data should be a GeoJSON Feature with a Point geometry
+- `POST /items/:uuid`: send new location of item with UUID `:uuid`. POST data should be of the following form:
+
+```json
+{
+  "step": "stepId",
+  "feature": {
+    "type": "Feature",
+    "properties": {
+
+    },
+    "geometry": "geojsonGeometry"
+  }
+}
+```
 
 POST example:
 
     curl -X POST -H 'content-type:application/json' -d '{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[ -79.062595,43.083056]}}' http://localhost:3000/items/5e66b3e8-c8d9-d471-e040-e00a180654d7
+
+### Locations
+
+- `GET /locations`: get all geolocated items
+
+## Collections
+
+Where? currently currently only includes items from the collection [The Eno collection of New York City views](http://digitalcollections.nypl.org/collections/the-eno-collection-of-new-york-city-views#/?tab=about).
 
 ## Data
 
@@ -26,4 +51,4 @@ And, install [jq](https://stedolan.github.io/jq/):
 
 Then, download and parse a image collection:
 
-    digital-collections -u 22f5f390-c5f0-012f-2796-58d385a7bc34 > data.json | jq  '[.[] | {uuid: .uuid, title: .title, imageLink: .imageLinks.imageLink}]' > data/22f5f390-c5f0-012f-2796-58d385a7bc34.json
+    digital-collections -u 22f5f390-c5f0-012f-2796-58d385a7bc34 | jq  '[.[] | {uuid: .uuid, title: .title, imageLink: .imageLinks.imageLink}]' > data/22f5f390-c5f0-012f-2796-58d385a7bc34.json
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var R = require('ramda');
 var express = require('express');
 var cors = require('cors');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var geojsonhint = require('geojsonhint');
 var package = require('./package');
 var app = express();
 var pg = require('pg');
@@ -9,39 +10,79 @@ var pg = require('pg');
 app.use(bodyParser.json());
 app.use(cors());
 
-var pgConString = "postgres://postgres:postgres@localhost/where";
-
-// CREATE TABLE public.locations
-// (
-//   uuid text NOT NULL,
-//   session text NOT NULL,
-//   step text NOT NULL,
-//   properties json,
-//   geometry geometry,
-//   CONSTRAINT locations_pkey PRIMARY KEY (uuid, session, step)
-// )
-// WITH (
-//   OIDS=FALSE
-// );
-
 var PORT = process.env.PORT || 3000;
 
-var uuid = '22f5f390-c5f0-012f-2796-58d385a7bc34';
-collectionByUuid = {};
-var collection = require(`./data/${uuid}.json`)
-    .filter(function(item) {
-      return item.imageLink;
-    })
-    .map(function(item) {
-      item.imageLink = item.imageLink.filter(function(imageLink) {
-        return imageLink.includes('&t=w&');
-      })[0];
+var items = {};
+var uuids;
+var collections = [
+  '22f5f390-c5f0-012f-2796-58d385a7bc34'
+];
 
-      collectionByUuid[item.uuid] = item;
-      return item;
-    });
+R.flatten(collections.map(uuid => require(`./data/${uuid}.json`)))
+  .filter(function(item) {
+    return item.imageLink;
+  })
+  .forEach(function(item) {
+    item.imageLink = item.imageLink.filter(function(imageLink) {
+      return imageLink.includes('&t=w&');
+    })[0];
 
+    items[item.uuid] = item;
+  });
 
+uuids = R.keys(items);
+
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
+var pgConString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/where';
+function executeQuery(query, values, callback) {
+  pg.connect(pgConString, function(err, client, done) {
+    if (err) {
+      callback(err);
+    } else {
+      client.query(query, values, function(err, result) {
+        done();
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, result.rows);
+        }
+      });
+    }
+  });
+}
+
+var tableName = 'locations';
+
+var tableExists = `SELECT COUNT(*)
+  FROM pg_catalog.pg_tables
+  WHERE schemaname = 'public'
+  AND tablename  = '${tableName}'`;
+
+var createTable = `CREATE TABLE public.${tableName} (
+  uuid text NOT NULL,
+  session text NOT NULL,
+  step text NOT NULL,
+  properties json,
+  geometry json,
+  CONSTRAINT locations_pkey PRIMARY KEY (uuid, session, step)
+)`;
+
+executeQuery(tableExists, null, function(err, rows) {
+  if (err) {
+    console.error('Error connecting to database:', err.message);
+    process.exit(-1);
+  } else {
+    if (!(rows && rows[0].count === '1')) {
+      console.log(`Table "${tableName}" does not exist - creating table...`);
+      executeQuery(createTable, null, function(err) {
+        if (err) {
+          console.error('Error creating table:', err.message);
+          process.exit(-1);
+        }
+      });
+    }
+  }
+});
 
 app.get('/', function (req, res) {
   res.send({
@@ -51,29 +92,29 @@ app.get('/', function (req, res) {
 });
 
 app.get('/items', function (req, res) {
-  res.send(collection);
+  res.send(R.values(items));
+});
+
+app.get('/items/random', function (req, res) {
+  res.send(items[uuids[Math.floor(Math.random() * uuids.length)]]);
 });
 
 app.get('/items/:uuid', function (req, res) {
   var uuid = req.params.uuid;
-  if (!collectionByUuid[uuid]) {
+  if (!items[uuid]) {
     res.status(404).send({
       result: 'error',
       message: 'Not found'
     });
   } else {
-    res.send(collectionByUuid[uuid]);
+    res.send(items[uuid]);
   }
-});
-
-app.get('/items/random', function (req, res) {
-  res.send(collection[Math.floor(Math.random() * collection.length)]);
 });
 
 app.post('/items/:uuid', function (req, res) {
   var uuid = req.params.uuid;
 
-  if (!collectionByUuid[uuid]) {
+  if (!(uuid && items[uuid])) {
     res.status(404).send({
       result: 'error',
       message: 'Not found'
@@ -81,7 +122,15 @@ app.post('/items/:uuid', function (req, res) {
     return;
   }
 
-  // TODO: check if req.body is GeoJSON
+  var geojsonErrors = geojsonhint.hint(req.body.feature).map(e => e.message);
+  if (geojsonErrors.length > 0) {
+    res.status(406).send({
+      result: 'error',
+      message: geojsonErrors.length === 1 ? geojsonErrors[0] : geojsonErrors
+    });
+    return;
+  }
+
   var session = 1;
 
   var data = {
@@ -98,59 +147,39 @@ app.post('/items/:uuid', function (req, res) {
   var keys = R.keys(data);
   var placeholders = Array.from(new Array(keys.length), (x, i) => i + 1).map(i => `$${i}`);
 
-  pg.connect(pgConString, function(err, client, done) {
+  executeQuery(`INSERT INTO locations (${keys.join(', ')}) VALUES (${placeholders.join(', ')})`, R.values(data), function(err) {
     if (err) {
       res.status(500).send({
         result: 'error',
         message: err.message
       });
     } else {
-      client.query(`INSERT INTO locations (${keys.join(', ')}) VALUES (${placeholders.join(', ')})`, R.values(data), function(err, result) {
-        pgDone();
-
-        if (err) {
-          res.status(500).send({
-            result: 'error',
-            message: err.message
-          });
-        } else {
-          res.send({
-            result: 'success'
-          });
-        }
+      res.send({
+        result: 'success'
       });
     }
   });
 });
 
 app.get('/locations', function (req, res) {
-  pg.connect(pgConString, function(err, client, done) {
+  executeQuery(`SELECT * FROM locations`, null, function(err, rows) {
     if (err) {
       res.status(500).send({
         result: 'error',
         message: err.message
       });
     } else {
-      client.query(`SELECT * FROM locations`, function(err, result) {
-        if (err) {
-          res.status(500).send({
-            result: 'error',
-            message: err.message
-          });
-        } else {
-          res.send({
-            type: 'FeatureCollection',
-            features: result.rows.map(row => ({
-              type: 'Feature',
-              properties: {
-                uuid: row.uuid,
-                step: row.step,
-                url: 'http://digitalcollections.nypl.org/items/' + row.uuid
-              },
-              geometry: row.geometry
-            }))
-          });
-        }
+      res.send({
+        type: 'FeatureCollection',
+        features: rows.map(row => ({
+          type: 'Feature',
+          properties: {
+            uuid: row.uuid,
+            step: row.step,
+            url: 'http://digitalcollections.nypl.org/items/' + row.uuid
+          },
+          geometry: row.geometry
+        }))
       });
     }
   });
