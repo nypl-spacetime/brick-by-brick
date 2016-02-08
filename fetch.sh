558301ae988fea03fac972d254b294b31f4ca0a5
Bert Spaan <bert.spaan@gmail.com>
2016-02-08 17:01:45 -0500

    added imageid field

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 digital-collections -u $1 | \
-  jq  '[.[] | {uuid: .uuid, title: .title, imageLink: .imageLinks.imageLink}]' > \
+  jq  '[.[] | {uuid: .uuid, title: .title, imageID: .imageID, imageLink: .imageLinks.imageLink}]' > \
   data/$1.json
