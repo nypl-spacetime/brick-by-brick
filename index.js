7a653d4770540618888bfc62d9fe641738709109
Bert Spaan <bert.spaan@gmail.com>
2016-03-31 10:17:46 -0400

    standardjs!

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-var R = require('ramda');
-var express = require('express');
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ port: 9889 });
-var cors = require('cors');
-var jwt = require('jsonwebtoken');
-var request = require('request');
-var bodyParser = require('body-parser');
-var geojsonhint = require('geojsonhint');
+'use strict'
+
+var R = require('ramda')
+var express = require('express')
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ port: 9889 })
+var cors = require('cors')
+var jwt = require('jsonwebtoken')
+var request = require('request')
+var bodyParser = require('body-parser')
+var geojsonhint = require('geojsonhint')
 var turf = {
   centroid: require('turf-centroid')
-};
-var package = require('./package');
-var app = express();
-var pg = require('pg');
+}
+var wherePackage = require('./package')
+var app = express()
+var pg = require('pg')
 
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    client.send(data);
-  });
-};
+wss.broadcast = function broadcast (data) {
+  wss.clients.forEach(function each (client) {
+    client.send(data)
+  })
+}
 
-var collections = require('./data/collections.json');
+var collections = require('./data/collections.json')
 
 if (!process.env.WHERE_PRIVATE_KEY) {
-  console.error('Please set WHERE_PRIVATE_KEY environment variable!');
-  process.exit(-1);
+  console.error('Please set WHERE_PRIVATE_KEY environment variable!')
+  process.exit(-1)
 }
 
 if (!process.env.DIGITAL_COLLECTIONS_TOKEN) {
-  console.error('Please set DIGITAL_COLLECTIONS_TOKEN environment variable to use /mods API');
+  console.error('Please set DIGITAL_COLLECTIONS_TOKEN environment variable to use /mods API')
 }
 
-var KEY = process.env.WHERE_PRIVATE_KEY;
-var DIGITAL_COLLECTIONS_TOKEN = process.env.DIGITAL_COLLECTIONS_TOKEN;
+var KEY = process.env.WHERE_PRIVATE_KEY
+var DIGITAL_COLLECTIONS_TOKEN = process.env.DIGITAL_COLLECTIONS_TOKEN
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 var headers = [
   'Accept',
@@ -43,7 +45,7 @@ var headers = [
   'Content-Length',
   'Connection',
   'X-Powered-By'
-];
+]
 
 app.use(cors({
   methods: [
@@ -52,65 +54,62 @@ app.use(cors({
   ],
   exposedHeaders: headers,
   allowedHeaders: headers
-}));
+}))
 
 // Enable CORS OPTIONS requests
 // https://github.com/expressjs/cors#enabling-cors-pre-flight
-app.options('*', cors());
+app.options('*', cors())
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000
 
-var items = {};
-var uuids;
+var items = {}
+var uuids
 
 // Load collection items for each collection in collections.json
 //   extent item data with UUID of collection itself
 var collectionData = collections
-  .filter(collection => !collection.exclude)
-  .map(collection =>
-    require(`./data/${collection.uuid}.json`)
-      .map(item => R.merge(item, {collection: collection.uuid}))
-  );
+  .filter((collection) => !collection.exclude)
+  .map((collection) => require(`./data/${collection.uuid}.json`)
+    .map((item) => R.merge(item, {collection: collection.uuid}))
+)
 
 R.flatten(collectionData)
-  .filter(function(item) {
-    return item.imageLink;
+  .filter((item) => item.imageLink)
+  .forEach((item) => {
+    item.imageLink = item.imageLink.filter(function (imageLink) {
+      return imageLink.includes('&t=w&')
+    })[0]
+
+    items[item.uuid] = item
   })
-  .forEach(function(item) {
-    item.imageLink = item.imageLink.filter(function(imageLink) {
-      return imageLink.includes('&t=w&');
-    })[0];
 
-    items[item.uuid] = item;
-  });
-
-uuids = R.keys(items);
+uuids = R.keys(items)
 
 // https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
-var pgConString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/where';
-function executeQuery(query, values, callback) {
-  pg.connect(pgConString, function(err, client, done) {
+var pgConString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/where'
+function executeQuery (query, values, callback) {
+  pg.connect(pgConString, function (err, client, done) {
     if (err) {
-      callback(err);
+      callback(err)
     } else {
-      client.query(query, values, function(err, result) {
-        done();
+      client.query(query, values, function (err, result) {
+        done()
         if (err) {
-          callback(err);
+          callback(err)
         } else {
-          callback(null, result.rows);
+          callback(null, result.rows)
         }
-      });
+      })
     }
-  });
+  })
 }
 
-var tableName = 'locations';
+var tableName = 'locations'
 
 var tableExists = `SELECT COUNT(*)
   FROM pg_catalog.pg_tables
   WHERE schemaname = 'public'
-  AND tablename  = '${tableName}'`;
+  AND tablename  = '${tableName}'`
 
 var createTable = `CREATE TABLE public.${tableName} (
   uuid text NOT NULL,
@@ -126,92 +125,92 @@ var createTable = `CREATE TABLE public.${tableName} (
   geometry json,
   centroid point,
   CONSTRAINT locations_pkey PRIMARY KEY (uuid, session, step)
-)`;
+)`
 
-executeQuery(tableExists, null, function(err, rows) {
+executeQuery(tableExists, null, function (err, rows) {
   if (err) {
-    console.error('Error connecting to database:', err.message);
-    process.exit(-1);
+    console.error('Error connecting to database:', err.message)
+    process.exit(-1)
   } else {
     if (!(rows && rows[0].count === '1')) {
-      console.log(`Table "${tableName}" does not exist - creating table...`);
-      executeQuery(createTable, null, function(err) {
+      console.log(`Table "${tableName}" does not exist - creating table...`)
+      executeQuery(createTable, null, function (err) {
         if (err) {
-          console.error('Error creating table:', err.message);
-          process.exit(-1);
+          console.error('Error creating table:', err.message)
+          process.exit(-1)
         }
-      });
+      })
     }
   }
-});
+})
 
 app.get('/', function (req, res) {
   res.send({
-    title: package.description,
-    version: package.version
-  });
-});
+    title: wherePackage.description,
+    version: wherePackage.version
+  })
+})
 
 app.get('/items', function (req, res) {
-  res.send(R.values(items));
-});
+  res.send(R.values(items))
+})
 
-function sendItem(req, res, uuid) {
+function sendItem (req, res, uuid) {
   if (!(uuid && items[uuid])) {
     res.status(404).send({
       result: 'error',
       message: 'Not found'
-    });
+    })
   } else {
-    res.send(items[uuid]);
+    res.send(items[uuid])
   }
 }
 
-function randomString() {
-  var length = 32;
-  var token = Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-  return token;
+function randomString () {
+  var length = 32
+  var token = Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1)
+  return token
 }
 
-function checkOrCreateToken(req, res, next) {
+function checkOrCreateToken (req, res, next) {
   if (!req.headers.authorization) {
-    var session = randomString();
-    var token = jwt.sign({ session: session }, KEY);
-    res.setHeader('Authorization', token);
+    var session = randomString()
+    var token = jwt.sign({ session: session }, KEY)
+    res.setHeader('Authorization', token)
   } else {
-    res.setHeader('Authorization', req.headers.authorization);
+    res.setHeader('Authorization', req.headers.authorization)
   }
-  next();
+  next()
 }
 
-function checkToken(req, res, next) {
+function checkToken (req, res, next) {
   if (!req.headers.authorization) {
     res.status(401).send({
       result: 'error',
       message: 'Not authorized'
-    });
+    })
   } else {
-    var token = req.headers.authorization;
-    jwt.verify(token, KEY, function(err, decoded) {
+    var token = req.headers.authorization
+    jwt.verify(token, KEY, function (err, decoded) {
       if (err) {
         res.status(401).send({
           result: 'error',
           message: 'Not authorized'
-        });
+        })
       } else {
-        req.session = decoded.session;
-        next();
+        req.session = decoded.session
+        next()
       }
-    });
+    })
   }
 }
 
-function emitEvent(row) {
-  var feature = locationToFeature(row);
-  wss.broadcast(JSON.stringify(feature));
+function emitEvent (row) {
+  var feature = locationToFeature(row)
+  wss.broadcast(JSON.stringify(feature))
 }
 
-function locationToFeature(row) {
+function locationToFeature (row) {
   return {
     type: 'Feature',
     properties: {
@@ -223,36 +222,36 @@ function locationToFeature(row) {
       data: row.data
     },
     geometry: row.geometry
-  };
+  }
 }
 
-function locationsToGeoJson(rows) {
+function locationsToGeoJson (rows) {
   return {
     type: 'FeatureCollection',
     features: rows.map(locationToFeature)
-  };
+  }
 }
 
 app.get('/items/random', checkOrCreateToken, function (req, res) {
-  var uuid = uuids[Math.floor(Math.random() * uuids.length)];
-  sendItem(req, res, uuid);
-});
+  var uuid = uuids[Math.floor(Math.random() * uuids.length)]
+  sendItem(req, res, uuid)
+})
 
 app.get('/items/:uuid', checkOrCreateToken, function (req, res) {
-  var uuid = req.params.uuid;
-  sendItem(req, res, uuid);
-});
+  var uuid = req.params.uuid
+  sendItem(req, res, uuid)
+})
 
 app.get('/items/:uuid/mods', function (req, res) {
-  var uuid = req.params.uuid;
+  var uuid = req.params.uuid
 
   if (!DIGITAL_COLLECTIONS_TOKEN) {
     res.status(401).send({
       result: 'error',
       message: 'Not authorized'
-    });
+    })
   } else {
-    var url = `http://api.repo.nypl.org/api/v1/items/mods_captures/${uuid}`;
+    var url = `http://api.repo.nypl.org/api/v1/items/mods_captures/${uuid}`
     request({
       url: url,
       json: true,
@@ -264,20 +263,20 @@ app.get('/items/:uuid/mods', function (req, res) {
         res.status(500).send({
           result: 'error',
           message: error
-        });
+        })
       } else {
         if (body && body.nyplAPI && body.nyplAPI.response && body.nyplAPI.response.mods) {
-          res.send(body.nyplAPI.response.mods);
+          res.send(body.nyplAPI.response.mods)
         } else {
           res.status(406).send({
             result: 'error',
             message: 'Cannot parse MODS result'
-          });
+          })
         }
       }
-    });
+    })
   }
-});
+})
 
 app.post('/items/:uuid', checkToken, function (req, res) {
   var row = {
@@ -291,18 +290,18 @@ app.post('/items/:uuid', checkToken, function (req, res) {
     client: null,
     geometry: null,
     centroid: null
-  };
+  }
 
   // Check if UUID exists in available collections, return 404 otherwise
   if (!(row.uuid && items[row.uuid])) {
     res.status(404).send({
       result: 'error',
       message: 'Not found'
-    });
-    return;
+    })
+    return
   }
 
-  row.image_id = items[row.uuid].imageID;
+  row.image_id = items[row.uuid].imageID
 
   // POST data should be GeoJSON feature (with optional geometry)
   //   properties should contain:
@@ -311,72 +310,72 @@ app.post('/items/:uuid', checkToken, function (req, res) {
   //     - completed
   //   if properties.completed == true,
   //     properties.data should contain step data
-  var feature = req.body;
+  var feature = req.body
 
   // Check if step and stepIndex are present in properties
   if (!(feature.properties.step && feature.properties.stepIndex >= 0)) {
     res.status(406).send({
       result: 'error',
       message: 'Feature should contain step and stepIndex'
-    });
-    return;
+    })
+    return
   }
-  row.step = feature.properties.step;
-  row.step_index = feature.properties.stepIndex;
+  row.step = feature.properties.step
+  row.step_index = feature.properties.stepIndex
 
   if (feature.properties.completed) {
     if (!feature.properties.data) {
       res.status(406).send({
         result: 'error',
         message: 'Completed steps should contain data'
-      });
-      return;
+      })
+      return
     }
-    row.completed = true;
-    row.data = JSON.stringify(feature.properties.data);
+    row.completed = true
+    row.data = JSON.stringify(feature.properties.data)
   } else {
     if (feature.properties.data || feature.properties.data) {
       res.status(406).send({
         result: 'error',
         message: 'Only completed steps should contain data or geometry'
-      });
-      return;
+      })
+      return
     }
 
-    row.completed = false;
+    row.completed = false
   }
 
   if (feature.geometry) {
-    var geojsonErrors = geojsonhint.hint(feature).map(e => e.message);
+    var geojsonErrors = geojsonhint.hint(feature).map((err) => err.message)
     if (geojsonErrors.length > 0) {
       res.status(406).send({
         result: 'error',
         message: geojsonErrors.length === 1 ? geojsonErrors[0] : geojsonErrors
-      });
-      return;
+      })
+      return
     }
-    row.geometry = JSON.stringify(feature.geometry);
+    row.geometry = JSON.stringify(feature.geometry)
 
     // Compute centroid of geometry
-    var centroid = turf.centroid(feature.geometry);
+    var centroid = turf.centroid(feature.geometry)
     if (centroid) {
-      row.centroid = centroid.geometry.coordinates.join(',');
+      row.centroid = centroid.geometry.coordinates.join(',')
     }
   }
 
   // Get session ID, checkToken function stores correct sessions in req.session
-  row.session = req.session;
+  row.session = req.session
 
   // Get information about client
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   var client = {
     ip: ip
-  };
-  row.client = JSON.stringify(client);
+  }
+  row.client = JSON.stringify(client)
 
-  var columns = R.keys(row);
-  var placeholders = Array.from(new Array(columns.length), (x, i) => i + 1).map(i => `$${i}`);
-  var values = placeholders.join(', ');
+  var columns = R.keys(row)
+  var placeholders = Array.from(new Array(columns.length), (x, i) => i + 1).map((i) => `$${i}`)
+  var values = placeholders.join(', ')
   var query = `INSERT INTO locations (${columns.join(', ')})
 VALUES (${values})
 ON CONFLICT (uuid, session, step)
@@ -389,25 +388,25 @@ DO UPDATE SET
   client = EXCLUDED.client,
   geometry = EXCLUDED.geometry,
   centroid = EXCLUDED.centroid
-WHERE EXCLUDED.completed;`;
+WHERE EXCLUDED.completed;`
 
-  executeQuery(query, R.values(row), function(err) {
+  executeQuery(query, R.values(row), function (err) {
     if (err) {
       res.status(500).send({
         result: 'error',
         message: err.message
-      });
+      })
     } else {
       emitEvent(Object.assign(row, {
         data: feature.properties.data,
         geometry: feature.geometry
-      }));
+      }))
       res.send({
         result: 'success'
-      });
+      })
     }
-  });
-});
+  })
+})
 
 var locationsQuery = `
   SELECT * FROM (
@@ -416,38 +415,38 @@ var locationsQuery = `
     GROUP BY uuid, session
   ) AS s JOIN locations l ON l.step_index = max_step AND s.uuid = l.uuid AND s.session = l.session
   ORDER BY date_modified DESC
-`;
+`
 
 app.get('/locations', function (req, res) {
-  executeQuery(locationsQuery, null, function(err, rows) {
+  executeQuery(locationsQuery, null, function (err, rows) {
     if (err) {
       res.status(500).send({
         result: 'error',
         message: err.message
-      });
+      })
     } else {
-      res.send(locationsToGeoJson(rows));
+      res.send(locationsToGeoJson(rows))
     }
-  });
-});
+  })
+})
 
 app.get('/locations/latest', function (req, res) {
-  executeQuery(`${locationsQuery} LIMIT 100`, null, function(err, rows) {
+  executeQuery(`${locationsQuery} LIMIT 100`, null, function (err, rows) {
     if (err) {
       res.status(500).send({
         result: 'error',
         message: err.message
-      });
+      })
     } else {
-      res.send(locationsToGeoJson(rows));
+      res.send(locationsToGeoJson(rows))
     }
-  });
-});
+  })
+})
 
 app.get('/collections', function (req, res) {
-  res.send(collections);
-});
+  res.send(collections)
+})
 
 app.listen(PORT, function () {
-  console.log(`NYPL Where API listening on PORT ${PORT}!`);
-});
+  console.log(`NYPL Where API listening on PORT ${PORT}!`)
+})
